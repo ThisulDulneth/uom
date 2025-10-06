90c71bf9729e48939b9878080e752a865cee49d7
ThisulDulneth <thisuldulneth521@gmail.com>
2025-10-06 21:55:53 +0530

    eular 6

---
+++ eular6.py
@@ -0,0 +1,9 @@
+#  getting the square difference
+
+def square_difference(n):
+    sum_of_squares = sum([i**2 for i in range(1, n+1)])
+    square_of_sum =sum([i for i in range(1, n+1)])**2
+    return square_of_sum - sum_of_squares
+
+print(square_difference(100))
+
